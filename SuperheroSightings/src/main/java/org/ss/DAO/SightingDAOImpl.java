24c82620a6d38fb992d82933db23858fc73c4ce7
neyssacadet <neyssacadet@gmail.com>
2022-11-27 10:06:50 -0600

    created row mapper for sighting Dao

--- SuperheroSightings/src/main/java/org/ss/DAO/SightingDAOImpl.java
+++ SuperheroSightings/src/main/java/org/ss/DAO/SightingDAOImpl.java
@@ -1,8 +1,14 @@
 package org.ss.DAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.ss.DTO.Sighting;
 
 /**
@@ -13,10 +19,21 @@ import org.ss.DTO.Sighting;
 
 public class SightingDAOImpl implements SightingDAO{
 
+    private final JdbcTemplate jdbc;
+
+    @Autowired
+    public SightingDAOImpl(JdbcTemplate jdbc){
+        this.jdbc = jdbc;
+    }
+
     @Override
     public Sighting getSightingByID(int sightingID) {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            final String SQL = "SELECT * FROM Sightings WHERE sightingID = ?";
+            return jdbc.queryForObject(SQL, new LocationMapper(), sightingID);
+        } catch (DataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
@@ -47,6 +64,18 @@ public class SightingDAOImpl implements SightingDAO{
     public List<Sighting> getSightingsByDate(LocalDate date) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private static final class SightingMapper implements RowMapper<Sighting> {
+
+        @Override
+        public Sighting mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Sighting sighting = new Sighting();
+            sighting.setSightingID(rs.getInt("sightingID"));
+            sighting.setSightingDate(rs.getDate("sightingDate").toLocalDate());
+                       
+            return sighting;
+        }
     }
 
 
