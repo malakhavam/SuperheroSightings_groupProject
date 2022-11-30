aa7411afdc17edafd444f5d19048d968411989f9
neyssacadet <neyssacadet@gmail.com>
2022-11-30 08:49:55 -0600

    set up testing for PowerDao

--- SuperheroSightings/src/test/java/supersightings/Data/PowerDaoTest.java
+++ SuperheroSightings/src/test/java/supersightings/Data/PowerDaoTest.java
@@ -1,5 +1,72 @@
 package supersightings.Data;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.ss.DAO.*;
+import org.ss.DTO.*;
+
+import java.util.List;
+
 public class PowerDaoTest {
+    @Autowired
+    SuperDAO superDao;
+
+    @Autowired
+    LocationDAO locationDao;
+
+    @Autowired
+    OrganizationDAO organizationDao;
+
+    @Autowired
+    SightingDAO sightingDao;
+
+    @Autowired
+    PowerDAO powerDao;
+
+    public PowerDaoTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        List<Super> sups = superDao.getAllSupers();
+        for(Super sup : sups){
+            superDao.deleteSuper(sup.getSuperID());
+        }
+
+        List<Location> locations = locationDao.getAllLocations();
+        for(Location location : locations) {
+            locationDao.deleteLocation(location.getLocationID());
+        }
+
+        List<Organization> organizations = organizationDao.getAllOrganizations();
+        for(Organization organization : organizations){
+            organizationDao.deleteOrganization(organization.getOrganizationID());
+        }
+
+        List<Sighting> sightings = sightingDao.getAllSightings();
+        for(Sighting sighting : sightings) {
+            sightingDao.deleteSighting(sighting.getSightingID());
+        }
+
+        List<Power> powers = powerDao.getAllPowers();
+        for(Power power : powers){
+            powerDao.deletePower(power.getPowerID());
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
     
 }
